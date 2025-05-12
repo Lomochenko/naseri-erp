ed84d2e03d49376bd3948605a40c375ad771a608
Lomochenko <ali.naseri3179@gmail.com>
2025-05-12 14:57:37 +0330

    delete unnecessary files

--- run_with_log.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ===== فعال‌سازی محیط مجازی =====
-call venv\Scripts\activate
-echo ===== اجرای سرور با لاگ کامل =====
-python manage.py runserver --traceback > server_log.txt 2>&1
-pause
