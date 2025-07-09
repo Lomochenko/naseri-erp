84513386dee1c27eadbb2d52a28b7cdb5dae6fec
Lomochenko <ali.naseri3179@gmail.com>
2025-07-09 18:38:53 +0330

    configure structure of frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <router-view />
+  <ThemeProvider>
+    <SidebarProvider>
+      <RouterView />
+    </SidebarProvider>
+  </ThemeProvider>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import ThemeProvider from './components/layout/ThemeProvider.vue'
+import SidebarProvider from './components/layout/SidebarProvider.vue'
 </script>
-
-<style scoped>
-</style>
